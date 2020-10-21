920333a675b0ce7a4f876985ab806956e8bb5c7a
oussamaghzl <oussama.ghzl6@gmail.com>
2020-10-21 15:31:14 +0200

    enleve modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import Formulaire from './components/Formulaire';
-import Modalconnec from './components/Modalconnec';
 import Navbar from './components/navbar';
 import Profil from './components/Profil';
 
 export default function App() {
 
-  const Apparition = () => {
-    console.log('salut');
-    return (
-      <Modalconnec className='PopUpVisible'></Modalconnec>
+  
 
-    )
-  }
   return (
-    <div>
-      <button onClick={Apparition}> Appuyer ici pour ouvrir le modal</button>
 
-      <Navbar/>
+    <div>
+      <Navbar />
       <Formulaire />
       <Profil />
     </div>
+
   )
 }
